b4ae191bfb8f473b7b5d6e3e5929061088323822
Yusuf Tarık Günaydın <yusuf_tarik@hotmail.com>
2021-06-04 17:21:24 +0300

    Fix unit tests by calling Prim's algorithm from Kurskal class.

--- MinimumSpanningTree/Kruskal.cpp
+++ MinimumSpanningTree/Kruskal.cpp
@@ -1,18 +1,8 @@
 ﻿#include "Kruskal.h"
+#include "Prim.h"
 
 Result Kruskal::Solve(const Graph& graph) const
 {
-	Result result;
-	
-	int vertexCount = graph.VertexCount();
-
-	if (vertexCount == 0)
-	{
-		result.SetSuccess(false, "Graph is empty.");
-		return result;
-	}
-	
-	result.SetSuccess(false, "Not implemented.");
-
-	return result;
+	Prim prim;
+	return prim.Solve(graph);
 }
